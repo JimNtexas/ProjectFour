a54762927bd65087c36a605d8c0ad22e166198a5
JimNtexas <grayraven42@gmail.com>
2016-01-19 21:50:37 -0600

    interstitial ad implemented

--- FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ FinalProject/app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -1,13 +1,21 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v4.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 
 
 /**
@@ -15,7 +23,17 @@ import com.google.android.gms.ads.AdView;
  */
 public class MainActivityFragment extends Fragment {
 
+    private InterstitialAd interstitialAd;
+    private final static String TAG = "FreeMainFragment";
+    public boolean interstitialLoaded = false;
+
     public MainActivityFragment() {
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initInterstitial();
     }
 
     @Override
@@ -29,10 +47,72 @@ public class MainActivityFragment extends Fragment {
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
         AdRequest adRequest = new AdRequest.Builder()
                 .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+           //     .addTestDevice("46AB0275142A57459C7BF5472676AA8F")
                 .build();
         mAdView.loadAd(adRequest);
         return root;
     }
 
+    private void initInterstitial() {
+        Log.d(TAG, "initInterstitial");
+        interstitialAd = new InterstitialAd(getContext());
+        String adId = this.getString(R.string.interstital_add_unit_id);
+        interstitialAd.setAdUnitId(adId);
+        AdRequest.Builder adRequestBuilder = new AdRequest.Builder();
+        adRequestBuilder.addTestDevice(AdRequest.DEVICE_ID_EMULATOR);
+        interstitialAd.loadAd(adRequestBuilder.build());
+
+        // Set an AdListener.
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Log.d(TAG, "onAdLoaded");
+                interstitialLoaded = true;
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Proceed
+                Log.d(TAG, "onAdClosed");
+                ((MainActivity)getActivity()).ExecuteAsyncTask();
+            }
+        });
+    }
+
+    private void showInterstitial() {
+        if(interstitialLoaded) {
+            Log.d(TAG, "showing interstitialAd");
+            interstitialAd.show();
+        } else {
+            Log.d(TAG, "interstitialAd not loaded");
+            interstitialLoaded = false;
+            ((MainActivity)getActivity()).ExecuteAsyncTask();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        LocalBroadcastManager.getInstance(getContext()).unregisterReceiver(showInterstitial);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        interstitialLoaded = false;
+        IntentFilter iff= new IntentFilter(MainActivity.INTERSTITIAL_INTENT);
+        LocalBroadcastManager.getInstance(getContext()).registerReceiver(showInterstitial, iff);
+        initInterstitial();
+    }
+
+    private BroadcastReceiver showInterstitial= new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            showInterstitial();
+        }
+    };
 
 }
--- FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,7 +1,9 @@
 package com.udacity.gradle.builditbigger;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -13,6 +15,10 @@ import com.grayraven.displayjokes.DisplayJokeActivity;
 public class MainActivity extends ActionBarActivity implements JokeTransaction {
 
     private static final String TAG = "main.MainActivity";
+    public static final String  INTERSTITIAL_INTENT =  "Interstitial_intent";
+    private ProgressDialog dlg;
+    private int interCnt = 0;
+    private static final int interlimit = 4; //used by free version only
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,12 +51,33 @@ public class MainActivity extends ActionBarActivity implements JokeTransaction {
 
     public void tellJoke(View view){
        Log.i(TAG, "starting DisplayJoke intent");
-       new EndpointsAsyncTasks(this).execute();
+       if(BuildConfig.FREE_VERSION && interCnt == 0 ) {
+           Log.d(TAG, "Send interstitial broadcast");
+           Intent intent = new Intent(INTERSTITIAL_INTENT);
+           LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+       } else {
+           ExecuteAsyncTask();
+       }
 
+        if(BuildConfig.FREE_VERSION) {
+            interCnt = interCnt > interlimit ? 0 : (interCnt + 1); //only show the interstitial periodically
+        }
+    }
+
+    public void ExecuteAsyncTask() {
+        new EndpointsAsyncTasks(this).execute();
+        dlg = new ProgressDialog(this);
+        dlg.setMessage(getString(R.string.progress_msg));
+        dlg.setCancelable(false);
+        dlg.show();
     }
 
     @Override
     public void jokeReady(String joke) {
+        Log.d(TAG, "jokeReady");
+        if(dlg != null) {
+            dlg.dismiss();
+        }
         Intent displayIntent = new Intent(this, DisplayJokeActivity.class);
         displayIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         displayIntent.putExtra("jokeText", joke);
